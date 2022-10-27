ad35e6c94cc189769a62d122f13263738f282fdb
mjespinoza <mjespinoza2@uc.cl>
2022-10-27 20:20:52 -0300

    fix subprojects

--- T2/Client/obj/Debug/net6.0/Client.AssemblyInfo.cs
+++ T2/Client/obj/Debug/net6.0/Client.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Servidor")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Servidor")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Servidor")]
+[assembly: System.Reflection.AssemblyProductAttribute("Client")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
--- T2/Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
+++ T2/Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Cliente")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Cliente")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Cliente")]
+[assembly: System.Reflection.AssemblyProductAttribute("Server")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
